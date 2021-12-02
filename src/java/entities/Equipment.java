79034600676721f917bd85d61733a41cbf5d281d
aitorp28 <62764832+aitorp28@users.noreply.github.com>
2021-12-02 10:01:17 +0100

    Equipment Entiti

---
+++ src/java/entities/Equipment.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entities;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Objects;
+import java.util.Set;
+import static javax.persistence.CascadeType.ALL;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+/**
+ * Entidad Equipment 
+ * @author Aitor Perez
+ */
+@Entity
+@Table(name = "EQUIPMENT")
+public class Equipment implements Serializable {
+    
+    
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String description;
+    @Temporal (TemporalType.TIMESTAMP)
+    private Timestamp dateAdd;
+    private Double cost;
+    @OneToMany(cascade = ALL , mappedBy = "equipment")
+  //  private Set<EventEquipment> events;
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getDateAdd() {
+        return dateAdd;
+    }
+
+    public void setDateAdd(Timestamp dateAdd) {
+        this.dateAdd = dateAdd;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+  /*  public Set<EventEquipment> getEvents() {
+        return events;
+    }
+
+    public void setEvents(Set<EventEquipment> events) {
+        this.events = events;
+    }*/
+
+    @Override
+    public String toString() {
+        return "Equipment{" + "id=" + id + ", description=" + description + ", dateAdd=" + dateAdd + ", cost=" + cost +
+                /*", events=" + events +*/ '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        hash = 67 * hash + Objects.hashCode(this.description);
+        hash = 67 * hash + Objects.hashCode(this.dateAdd);
+        hash = 67 * hash + Objects.hashCode(this.cost);
+      //  hash = 67 * hash + Objects.hashCode(this.events);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Equipment other = (Equipment) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.dateAdd, other.dateAdd)) {
+            return false;
+        }
+        if (!Objects.equals(this.cost, other.cost)) {
+            return false;
+        }
+    /*    if (!Objects.equals(this.events, other.events)) {
+            return false;
+        }*/
+        return true;
+    }
+   
+    
+    
+}
