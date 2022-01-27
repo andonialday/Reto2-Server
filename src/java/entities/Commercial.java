2f1ff25d7088d28ed664d051c9b6e16e8ae75b14
andonialday <andalday@gmail.com>
2022-01-27 10:37:54 +0100

    Missing import

--- src/java/entities/Commercial.java
+++ src/java/entities/Commercial.java
@@ -16,6 +16,7 @@ import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 @NamedQueries({
     @NamedQuery(
